c4bf6e9b20e39249a98e91ac4ddf3087f35c53ac
talsag-dev <talsagie19@gmail.com>
2022-03-13 13:03:06 +0200

    fileisnert

--- talsagie.java
+++ talsagie.java
@@ -2,8 +2,6 @@ public class Hello{
 public static void main(String[] args) {
 for (int i=1;i<=10; i++) {
 System.out.println("Tal Sagie");
-System.out.println("Tal Sagie");
-System.out.println("Tal Sagie");
 }
 }
 }
